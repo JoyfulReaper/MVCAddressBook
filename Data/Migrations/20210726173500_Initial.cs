bf5cf042ca9f00767e4d240ac012f00af5514c1e
Kyle Givler <xvegan88x@gmail.com>
2021-07-26 15:13:30 -0400

    Initial Working Project

---
+++ Data/Migrations/20210726173500_Initial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AddressBook.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Address1 = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Address2 = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    City = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    State = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Zip = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    ImageData = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    ImageType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        [NotMapped]
+        public string FullName
+        {
+            get => $"{FirstName} {LastName}";
+        }
+
+
+        [Required]
+        [MaxLength(200)]
+        public string Address1 { get; set; }
+
+        [MaxLength(200)]
+        public string Address2 { get; set; }
+
+        [MaxLength(100)]
+        public string City { get; set; }
+
+        [MaxLength(50)]
+        public string State { get; set; }
+
+        [DataType(DataType.PostalCode)]
+        public int Zip { get; set; }
+
+        [MaxLength(200)]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [MaxLength(20)]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+
+
+        [NotMapped]
+        [Display(Name = "Image")]
+        [DataType(DataType.Upload)]
+        public IFormFile ImageFile { get; set; }
+
+        public byte[] ImageData { get; set; }
+
+        public string ImageType { get; set; }
+
+
+        public DateTime Created { get; set; }
+    }
+}
